2282bbc6381a4ee817e21ed0fd030b0c55d42f64
Yan Brito <yan.briito@hotmail.com>
2021-10-07 02:28:58 -0300

    Feature: added tab bar navigation

--- App.tsx
+++ App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
+import { NavigationContainer } from '@react-navigation/native';
+
 import { ThemeProvider } from 'styled-components/native';
 
 import {
@@ -10,7 +12,7 @@ import {
 } from '@expo-google-fonts/poppins';
 
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -25,7 +27,9 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Register />
+      <NavigationContainer>
+        <AppRoutes/>
+      </NavigationContainer>
     </ThemeProvider>
   );
 };
